6cdf4b9420efe5b961f8e0617b4917d436d59109
AlexOsinovi <aliaksei.asinavy@innowise.com>
2025-06-21 01:04:28 +0300

    Adding MyLinkedList methods

---
+++ src/main/java/by/osinovi/my_linked_list/LinkedList.java
@@ -0,0 +1,143 @@
+package by.osinovi.my_linked_list;
+
+public class LinkedList<T> {
+    private class Node {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(T element) {
+        Node newNode = new Node(element);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addLast(T element) {
+        Node newNode = new Node(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+        Node newNode = new Node(element);
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+
+    public T getFirst() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        return head.data;
+    }
+
+    public T getLast() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public T removeFirst() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        T data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+
+    public T removeLast() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        if (size == 1) {
+            T data = head.data;
+            head = null;
+            size--;
+            return data;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        T data = current.next.data;
+        current.next = null;
+        size--;
+        return data;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        T data = current.next.data;
+        current.next = current.next.next;
+        size--;
+        return data;
+    }
+}
+
+
+
